4bbda7db44e0e2fa89b31dcdbcf705b904a4f4d6
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-09-25 22:54:55 +0200

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import '@/styles/globals.css'
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
+import { useState, useMemo } from 'react';
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  )
+}
